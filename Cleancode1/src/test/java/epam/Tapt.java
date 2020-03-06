b8ce0fd813d8f588808c7ca69a3ed1fabd708738
sharadha23 <60329643+sharadha23@users.noreply.github.com>
2020-03-06 08:32:08 +0530

    Add files via upload

---
+++ Cleancode1/src/test/java/epam/Tapt.java
@@ -0,0 +1,39 @@
+  
+package epam;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Tapt 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Tapt( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite(Tapt.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
